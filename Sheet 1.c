6175698f097694ef2391be3e6eadf87064b6b677
Aj shihab <149856067+aj2617@users.noreply.github.com>
2023-11-24 13:49:37 +0800

    Update Sheet 1.c

--- Sheet 1.c
+++ Sheet 1.c
@@ -4,7 +4,7 @@ int main()
 {
   int n;
   scanf("%d",&n);
-  if((n%4==0 && n%100!=0)||(n%400==0))
+If ((n % 400 ==0) || (n % 4==0 && n % 100!=0))
    printf("%d is leap Year",n);
   else
    printf("%d is not Leap year",n);
